7461f943bdc5cdf3db76ac255ea29e0e0164a996
maritzarahayu <maritzarahayui@gmail.com>
2024-08-13 14:11:15 +0700

    change imagee

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
    <div class="flex flex-col items-center justify-center min-h-screen">
-    <img alt="Vue logo" src="/assets/logo.png" class="mb-4">
+    <br>
+    <img alt="Vue logo" src="https://seeklogo.com/images/V/vuejs-logo-17D586B587-seeklogo.com.png" class="mb-4">
     <auth />
   </div>
 </template>
